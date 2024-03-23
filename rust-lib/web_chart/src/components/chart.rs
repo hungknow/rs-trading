24afc237185fdc32d0dcf4cf305a6c2f661234f3
hungbuiknow <hungbuiknow@gmail.com>
2024-03-24 00:20:06 +0700

    Add example to display chart by echarts

--- rust-lib/web_chart/src/components/chart.rs
+++ rust-lib/web_chart/src/components/chart.rs
@@ -1,19 +1,27 @@
-use leptos::{component, create_signal, view, IntoView};
-
-use crate::components::ChartControl;
+use crate::components::{ohlc::ohlc_chart, shanhai_index::shanhai_chart, ChartControl};
+use charming::WasmRenderer;
+use leptos::{component, create_action, create_effect, view, IntoView};
 
 #[component]
 pub fn Chart() -> impl IntoView {
-    // let (candles_signal, set_candles) = create_signal(Vec<int>::new());
+    // let action = create_action(|_input: &()| async {
+    //    let chart = shanhai_chart();
 
-    // let candles = create_resource(
-    //     move || candles_signal.get(),
-    //     move || async move { load_candles("mock:xauusd".to_owned(), "m1".to_owned(), 0, 12).await },
-    // );
+    //     let renderer = WasmRenderer::new(1200, 700);
+    //     renderer.render("chart",&chart).unwrap();
+    // });
+    create_effect(move |_| {
+        let chart = ohlc_chart();
+
+        let renderer = WasmRenderer::new(1200, 700);
+        renderer.render("chart", &chart).unwrap();
+    });
+
     view! {
         <section>
             <ChartControl />
-            // Display the c
+            // <button on:click=move |_| {action.dispatch(());}>"Show chart"</button>
+            <div id="chart"></div>
         </section>
     }
 }
--- rust-lib/web_chart/src/components/mod.rs
+++ rust-lib/web_chart/src/components/mod.rs
@@ -5,3 +5,5 @@ mod date_time_picker;
 pub use chart_control::*;
 pub use chart::*;
 pub use date_time_picker::*;
+mod ohlc;
+mod shanhai_index;
